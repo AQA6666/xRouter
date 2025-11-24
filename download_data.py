79dd917c73e332bab9baa5ba93a336b136cc856a
AQA6666 <60868870+AQA6666@users.noreply.github.com>
2025-11-24 18:10:15 +0800

    debug data download

--- download_data.py
+++ download_data.py
@@ -2,7 +2,7 @@ import json
 from datasets import load_dataset
 
 # Load dataset
-train_data = load_dataset("LLM360/guru-RL-92k", split="train", streaming=True, cache_dir="/gemini/space/Datasets/guru-RL-92k")
+train_data = load_dataset("LLM360/guru-RL-92k", split="train", cache_dir="/gemini/space/Datasets/guru-RL-92k")
 
 print(f"Columns: {train_data.column_names}")
 print(f"First item: {next(iter(train_data))}")
